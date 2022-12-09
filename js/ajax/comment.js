01e60770edb003e9abbf068000016c96b4f3ad80
CuDinh03 <dinhhuycu0305@gmail.com>
2022-12-10 00:23:02 +0700

    day 2

--- js/ajax/comment.js
+++ js/ajax/comment.js
@@ -1,28 +1,31 @@
-$(document).ready(()=> {
-    $("#review-user").click(() => {
-        let content = $("#comments").val()
+function saveComment(){
 
-        let reviews ={
+        let content = document.getElementById("comments").value
+
+        console.log(content)
+
+        let reviews = {
             "text": content
         }
 
         $.ajax({
-            type:"POST",
-            url: "http://localhost:8080/comment",
+            type: "POST",
+            url: "http://localhost:8080/comments",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
             data: JSON.stringify(reviews),
             success: function () {
                 alert("succesfully comment")
             },
-            error: function (error){
+            error: function (error) {
                 alert("error comment")
             }
 
         })
+}
 
-    })
-
-
-})
 
 
 
